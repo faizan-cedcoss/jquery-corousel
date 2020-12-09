b8a07f64ed1c1db724192260c05b668808e3fd4d
faizan7275 <74813450+faizan7275@users.noreply.github.com>
2020-12-09 19:36:39 +0530

    Add files via upload

---
+++ todo.js
@@ -0,0 +1,41 @@
+$(document).ready(function()
+{
+	$(document).on("click",".check",finishItem);
+	$(document).on("click",".edit",editItem);
+	$("#add").click(function(){
+	var a=$("#ques").val();
+	if(a.length!=0)	
+	{
+	$("#todo").append('<li><input type="checkbox" class="check">&emsp;<span class="span1">'+a+'</span>&emsp; <button class="edit">Edit</button><button class="del">Delete</button></li>');
+	$(".del").click(function(){		
+		var b=$(this).parent();
+		b.remove();		
+	})
+	}
+	})
+})
+function finishItem()
+	{
+	if($(this).parent().parent().attr('id')=="done")
+	{ let x= $(this).parent();
+	$("#todo").append(x);
+	}
+	else
+	{
+	  let y= $(this).parent();
+	$("#done").append(y);
+	}
+	}
+	function editItem()
+	{
+		var b1=$(this).parent();
+		var b3=b1.text();
+		var b2=b3.replace("EditDelete","");
+		var a1='&emsp; <input type=text value='+b2+'class="edit1" > &emsp; <button class="btn">done</button>'; 
+		$(b1).append(a1);
+		$(".btn").click(function(){
+			$(this).parent().children(".span1").text($(".edit1").val());
+			$(".edit1").remove();
+			$(".btn").remove();
+		})
+	}
